2e6383c38e7ad00d75cffcbb0ca9210cef85b470
KrystianOg <krystian@ogonowski.it>
2024-05-06 13:38:20 +0200

    cleanup
    shadcn cleanup
    
    decided to implement components on my own

--- apps/frontend/app/layout.tsx
+++ apps/frontend/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import {ThemeProvider} from 'next-themes'
 import { Header } from "@/components/main/header";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -19,10 +18,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-          <Header/>
-          {children}
-        </ThemeProvider>
+        <Header/>
+        {children}
       </body>
     </html>
   );
--- apps/frontend/components/theme-toggle.tsx
+++ apps/frontend/components/theme-toggle.tsx
@@ -1,31 +1,30 @@
 'use client'
 import {Button} from '@/components/ui/button'
-import {DropdownMenu, DropdownMenuItem, DropdownMenuContent, DropdownMenuTrigger} from '@/components/ui/dropdown-menu'
-import { SunIcon, MoonIcon } from 'lucide-react'
-import { useTheme } from 'next-themes'
+
+type Theme = "light" | "dark" | "system"
 
 export function ThemeToggle() {
-  const { setTheme } = useTheme();
+  /**
+   * TODO: change the implementation from next-themes to own
+   */
+  const setTheme = (theme: Theme) => {
+    // TODO: set global context and persist state in local storage
+  }
+
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
-          <SunIcon className="transition-all scale-100 dark:-rotate-90 dark:scale-0"/>
-          <MoonIcon className="absolute transition-all rotate-90 scale-0 dark:rotate-0 dark:scale-100"/>
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")}>
-          Light
-        </DropdownMenuItem>
-          <DropdownMenuItem onClick={() => setTheme("dark")}>
-          Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
-          System
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <div>
+      <button>trigger</button>
+      <ul>
+        <li>
+          <Button onClick={() => setTheme("light")}>light</Button>
+        </li>
+        <li>
+          <Button onClick={() => setTheme("dark")}>dark</Button>
+        </li>
+        <li>
+          <Button onClick={() => setTheme("system")}>system</Button>
+        </li>
+      </ul>
+    </div>
   )
 }
