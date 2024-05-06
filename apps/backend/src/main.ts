8ffb580a19cf91a83d38c9948bfd359c837c9210
KrystianOg <krystian@ogonowski.it>
2024-05-06 13:36:38 +0200

    enable cors for frontend

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ["http://localhost:3000"],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Origin', 'Accept', 'Autrhorization'],
+    exposedHeaders: ['Authorization'],
+    credentials: true
+  })
+  app.use(cookieParser());
 
   const config = new DocumentBuilder()
     .setTitle('POG cars API docs')
