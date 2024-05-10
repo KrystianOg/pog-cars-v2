18b11eb33a93e83097bda93abf61fc74a9f2e95d
KrystianOg <krystian@ogonowski.it>
2024-05-10 02:25:26 +0200

    install sharp for image optimization

--- apps/frontend/next.config.mjs
+++ apps/frontend/next.config.mjs
@@ -1,4 +1,14 @@
+import * as packageJson from './package.json' with {type: "json"}
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    config.externals.sharp = "commonjs sharp"
+    return config
+  },
+  env: {
+    version: packageJson.version
+  }
+};
 
 export default nextConfig;
