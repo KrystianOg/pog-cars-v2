4acfd1d76e7a9cfcf6e3d036cd434520f4e6d9a9
KrystianOg <krystian@ogonowski.it>
2024-05-10 02:27:13 +0200

    use navbar in layout

--- apps/frontend/app/(cars)/components/table/data-table.tsx
+++ apps/frontend/app/(cars)/components/table/data-table.tsx
@@ -1,114 +1,114 @@
-"use client";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
-import {
-  ColumnDef,
-  useReactTable,
-  getCoreRowModel,
-  flexRender,
-} from "@tanstack/react-table";
-import Link from "next/link";
+  /* "use client";
+  import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+  } from "@/components/ui/table";
+  import {
+    ColumnDef,
+    useReactTable,
+    getCoreRowModel,
+    flexRender,
+  } from "@tanstack/react-table";
+  import Link from "next/link";
 
-interface CarsTableProps<TData, TValue> {
-  columns: ColumnDef<TData, TValue>[];
-  data: TData[];
-}
+  interface CarsTableProps<TData, TValue> {
+    columns: ColumnDef<TData, TValue>[];
+    data: TData[];
+  }
 
-type Car = {
-  id: number;
-  make: string;
-  model: string;
-  imgUrl?: string;
-};
+  type Car = {
+    id: number;
+    make: string;
+    model: string;
+    imgUrl?: string;
+  };
 
-export const mockData: Car[] = [
-  {
-    id: 1,
-    make: "opel",
-    model: "astra",
-  },
-  {
-    id: 2,
-    make: "honda",
-    model: "civic",
-  },
-];
+  export const mockData: Car[] = [
+    {
+      id: 1,
+      make: "opel",
+      model: "astra",
+    },
+    {
+      id: 2,
+      make: "honda",
+      model: "civic",
+    },
+  ];
 
-export const columns: ColumnDef<Car>[] = [
-  {
-    accessorKey: "id",
-  },
-  {
-    accessorKey: "model",
-    header: "Model",
-  },
-  {
-    accessorKey: "make",
-    header: "Make",
-  },
-];
+  export const columns: ColumnDef<Car>[] = [
+    {
+      accessorKey: "id",
+    },
+    {
+      accessorKey: "model",
+      header: "Model",
+    },
+    {
+      accessorKey: "make",
+      header: "Make",
+    },
+  ];
 
-export function CarsTable<TData, TValue>({
-  columns,
-  data,
-}: CarsTableProps<TData, TValue>) {
-  const table = useReactTable({
-    data,
+  export function CarsTable<TData, TValue>({
     columns,
-    getCoreRowModel: getCoreRowModel(),
-  });
+    data,
+  }: CarsTableProps<TData, TValue>) {
+    const table = useReactTable({
+      data,
+      columns,
+      getCoreRowModel: getCoreRowModel(),
+    });
 
-  return (
-    <Table>
-      <TableHeader>
-        {table.getHeaderGroups().map((headerGroup) => (
-          <TableRow key={headerGroup.id}>
-            {headerGroup.headers.map((header) => (
-              <TableHead key={header.id}>
-                {header.isPlaceholder
-                  ? null
-                  : flexRender(
-                      header.column.columnDef.header,
-                      header.getContext(),
-                    )}
-              </TableHead>
-            ))}
-          </TableRow>
-        ))}
-      </TableHeader>
-      <TableBody>
-        {table.getRowModel().rows?.length ? (
-          table.getRowModel().rows.map((row) => (
-            <TableRow
-              key={row.id}
-              data-state={row.getIsSelected() && "selected"}
-            >
-              {row.getVisibleCells().map((cell) => (
-                <TableCell key={cell.id} className="p-0">
-                  <Link
-                    href={`/car/${row.getValue("id")}`}
-                    className="p-4 block"
-                  >
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                  </Link>
-                </TableCell>
+    return (
+      <Table>
+        <TableHeader>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <TableRow key={headerGroup.id}>
+              {headerGroup.headers.map((header) => (
+                <TableHead key={header.id}>
+                  {header.isPlaceholder
+                    ? null
+                    : flexRender(
+                        header.column.columnDef.header,
+                        header.getContext(),
+                      )}
+                </TableHead>
               ))}
             </TableRow>
-          ))
-        ) : (
-          <TableRow>
-            <TableCell colSpan={columns.length} className="h-24 text-center">
-              No results.
-            </TableCell>
-          </TableRow>
-        )}
-      </TableBody>
-    </Table>
-  );
-}
+          ))}
+        </TableHeader>
+        <TableBody>
+          {table.getRowModel().rows?.length ? (
+            table.getRowModel().rows.map((row) => (
+              <TableRow
+                key={row.id}
+                data-state={row.getIsSelected() && "selected"}
+              >
+                {row.getVisibleCells().map((cell) => (
+                  <TableCell key={cell.id} className="p-0">
+                    <Link
+                      href={`/car/${row.getValue("id")}`}
+                      className="p-4 block"
+                    >
+                      {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                    </Link>
+                  </TableCell>
+                ))}
+              </TableRow>
+            ))
+          ) : (
+            <TableRow>
+              <TableCell colSpan={columns.length} className="h-24 text-center">
+                No results.
+              </TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    );
+  } */
--- apps/frontend/app/layout.tsx
+++ apps/frontend/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Header } from "@/components/main/header";
+import { NavbarDesktop } from "@/components/main/navbar/desktop";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Header />
+        <NavbarDesktop/>
         {children}
       </body>
     </html>
