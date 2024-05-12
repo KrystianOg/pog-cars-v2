944881aaeca4135d7fadfe0ef1b699d0045951ab
KrystianOg <krystian@ogonowski.it>
2024-05-12 22:40:47 +0200

    initial email templates

--- packages/transactional/emails/InviteUser.tsx
+++ packages/transactional/emails/InviteUser.tsx
@@ -1,4 +1,17 @@
-import { Body, Link, Container, Text, Head, Heading, Html, Button, Preview, Section, Img, Hr } from "@react-email/components";
+import {
+  Body,
+  Link,
+  Container,
+  Text,
+  Head,
+  Heading,
+  Html,
+  Button,
+  Preview,
+  Section,
+  Img,
+  Hr,
+} from "@react-email/components";
 import * as React from "react";
 
 const baseUrl = process.env.BASE_URL
@@ -7,86 +20,89 @@ const baseUrl = process.env.BASE_URL
 
 interface InviteUserProps {
   agency: {
-    name: string
-  },
+    name: string;
+  };
   user: {
-    email: string,
-    name?: string,
-  },
+    email: string;
+    name?: string;
+  };
   invitedBy: {
-    email: string,
-    name?: string,
-  },
-  inviteLink: string
+    email: string;
+    name?: string;
+  };
+  inviteLink: string;
 }
 
-export const InviteUser = ({agency, user, invitedBy, inviteLink}: InviteUserProps) => {
+export const InviteUser = ({
+  agency,
+  user,
+  invitedBy,
+  inviteLink,
+}: InviteUserProps) => {
   return (
     <Html>
-      <Head/>
+      <Head />
       <Preview>Team invitation</Preview>
       <Body style={{}}>
         <Container>
           <Section>
             {/* https://www.needpix.com/photo/download/1639487/car-car-design-car-vector-vehicle-automotive-sporty-technology-modern-transportation */}
-            <Img src={`${baseUrl}/static/logo.jpg`}/> {/* TODO: add img here*/}
-          </Section> 
+            <Img src={`${baseUrl}/static/logo.jpg`} /> {/* TODO: add img here*/}
+          </Section>
           <Heading>
             Join <strong>{agency.name}</strong> on <strong>POG cars</strong>
           </Heading>
+          <Text>Hello {user.name ?? user.email},</Text>
           <Text>
-            Hello {user.name ?? user.email},
-          </Text>
-          <Text>
-            {invitedBy.name ? 
+            {invitedBy.name ? (
               <>
                 <strong>{invitedBy.name}</strong> (
                 <Link href={`mailto:${invitedBy.email}`}>
                   {invitedBy.email}
                 </Link>
-                )            
-              </> : <Link href={`mailto:${invitedBy.email}`}>
-                {invitedBy.email}
-              </Link>}
-            {" "}has invited you to the <strong>{agency.name}</strong> agency on{" "}
-              <strong>POG cars</strong>
+                )
+              </>
+            ) : (
+              <Link href={`mailto:${invitedBy.email}`}>{invitedBy.email}</Link>
+            )}{" "}
+            has invited you to the <strong>{agency.name}</strong> agency on{" "}
+            <strong>POG cars</strong>
           </Text>
           <Section>
             <Button href={inviteLink}>Join the team</Button>
             <Text>
               or copy and paste this URL into your browser:{" "}
-              <Link href={inviteLink}>
-                {inviteLink}
-              </Link>
+              <Link href={inviteLink}>{inviteLink}</Link>
             </Text>
-            <Hr/>
+            <Hr />
             <Text>
               This invitation was intended for{" "}
-              <span>{user.name ?? user.email}</span>{" "}
-              If you were not expecting this invitation, you can ignore this email. If you are concerned about your account's safety, please reply to this email to get in touch with us.
+              <span>{user.name ?? user.email}</span> If you were not expecting
+              this invitation, you can ignore this email. If you are concerned
+              about your account's safety, please reply to this email to get in
+              touch with us.
             </Text>
           </Section>
         </Container>
       </Body>
-
     </Html>
   );
-}
+};
 
 InviteUser.PreviewProps = {
-  inviteLink: 'http://localhost:3000/agencies/invite?u=cfd25111-9b9d-4e1c-95dd-81a0c1e502b3',
+  inviteLink:
+    "http://localhost:3000/agencies/invite?u=cfd25111-9b9d-4e1c-95dd-81a0c1e502b3",
   agency: {
-    name: 'pOg',
+    name: "pOg",
   },
   user: {
-    name: 'John',
-    email: 'john.doe@mail.com',
+    name: "John",
+    email: "john.doe@mail.com",
   },
   invitedBy: {
-    name: 'Marta',
-    email: 'marta.lenore@mail.com'
-  }
-
-} satisfies InviteUserProps
+    name: "Marta",
+    email: "marta.lenore@mail.com",
+  },
+} satisfies InviteUserProps;
 
 export default InviteUser;
--- packages/transactional/emails/VerifyEmail.tsx
+++ packages/transactional/emails/VerifyEmail.tsx
@@ -1,28 +1,31 @@
-import { Body, Container, Text, Head, Heading, Html, Preview } from "@react-email/components";
+import {
+  Body,
+  Container,
+  Text,
+  Head,
+  Heading,
+  Html,
+  Preview,
+} from "@react-email/components";
 import * as React from "react";
 
 interface VerifyEmailProps {
-  email: string
+  email: string;
 }
 
-export const VerifyEmail = ({email}: VerifyEmailProps) => {
+export const VerifyEmail = ({ email }: VerifyEmailProps) => {
   return (
     <Html>
-      <Head/>
+      <Head />
       <Preview>Verify your email</Preview>
       <Body style={{}}>
         <Container>
-          <Heading>
-            Verify your email
-          </Heading>
-          <Text>
-            Hello {email}
-          </Text>
+          <Heading>Verify your email</Heading>
+          <Text>Hello {email}</Text>
         </Container>
       </Body>
-
     </Html>
   );
-}
+};
 
 export default VerifyEmail;
