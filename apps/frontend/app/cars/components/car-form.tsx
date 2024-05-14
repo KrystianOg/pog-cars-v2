7968ea3765cbd5be659462a98758d06a6a9792d3
KrystianOg <krystian@ogonowski.it>
2024-05-14 02:26:35 +0200

    move cars list fetch & cars form fixes #1

--- apps/frontend/app/cars/components/car-form.tsx
+++ apps/frontend/app/cars/components/car-form.tsx
@@ -6,47 +6,56 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import styles from "./car-form.module.css";
 
-import { carSchema as carFormSchema } from "../../../../backend/src/cars/cars.schema";
+import { carCreateSchema as carFormSchema } from "../../../../backend/src/cars/cars.schema";
+import { useEffect } from "react";
+import { Label } from "@/components/ui/label";
 
 interface CarFormProps {
   defaultValues?: z.infer<typeof carFormSchema>;
 }
 
 export function CarForm({ defaultValues }: CarFormProps) {
-  const { register, handleSubmit } = useForm<z.infer<typeof carFormSchema>>({
+  const { register, handleSubmit, formState} = useForm<z.infer<typeof carFormSchema>>({
     resolver: zodResolver(carFormSchema),
     defaultValues,
   });
 
+  console.log('form state', formState)
+
   const onSubmit = handleSubmit((values) => {
     console.log("car form values", values);
-
-    if (values.id) {
-      // TODO update db entry
-    } else {
-      // TODO create db entry
-    }
   });
+
+  useEffect(() => {
+
+    console.log(formState.isValid, formState.errors)
+  }, [formState])
+
 
   return (
     <form onSubmit={onSubmit} className={styles.form}>
-      <Input id="mileage" placeholder="Mileage" {...register("mileage")} />
+      <Input id="mileage" placeholder="Mileage" type="number" {...register("mileage")} />
+      <div>
+        <Label htmlFor="horsepower">Horsepower</Label>
+        <Input
+          id="horsepower"
+          placeholder="Horsepower"
+          type="number"
+          {...register("horsepower")}
+        />
+      </div>
+      <Input id="seats" placeholder="Seats" type="number" min={1} {...register("seats")} />
       <Input
-        id="horsepower"
-        placeholder="Horsepower"
-        {...register("horsepower")}
-      />
-      <Input id="seats" placeholder="Seats" {...register("seats")} />
-      <Input
+        // TODO: make it select
         id="drivetrain"
         placeholder="Drivetrain"
         {...register("drivetrain")}
       />
-      <Input id="price" placeholder="Price" {...register("price")} />
-      <Input id="year" placeholder="Production year" {...register("year")} />
+      <Input id="price" placeholder="Price" type="number" {...register("price")} />
+      <Input id="year" placeholder="Production year" type="number" min={1886} {...register("year")} />
       <Input id="model" placeholder="Model" {...register("model")} />
       <Input id="make" placeholder="Make" {...register("make")} />
-      <Button type="submit">Submit</Button>
+      <Button type="submit" disabled={!formState.isValid || !formState.isDirty} style={{gridColumn: '1/3'}}>Submit</Button>
     </form>
   );
 }
--- apps/frontend/app/cars/page.tsx
+++ apps/frontend/app/cars/page.tsx
@@ -1,7 +1,27 @@
 import { Dialog, DialogTrigger, DialogContent } from "@/components/ui/dialog";
 import { CarForm } from "./components/car-form";
+import { type Car } from "../../../backend/src/cars/cars.schema";
+import { get } from "@/lib/fetch";
+import Link from "next/link";
 
-export default function CarsPage() {
+export const revalidate = 300
+
+async function fetchData(): Promise<Car[]> {
+  const res = await get('cars');
+
+  if (!res || res.status >= 400) {
+    throw new Error()
+  }
+
+  const smth = await res.json()
+
+  console.log('smth', smth)
+  return smth
+}
+
+export default async function CarsPage() {
+  const cars = await fetchData()
+
   return (
     <main>
       Cars page here
@@ -11,6 +31,10 @@ export default function CarsPage() {
           <CarForm/>
         </DialogContent>
       </Dialog>
+      {"Cars: "}
+      {cars.map((car) => (
+        <Link href={`/cars/${car.id}`} key={car.id}>{car.make} {car.model}</Link>
+      ))}
     </main>
   );
 }
